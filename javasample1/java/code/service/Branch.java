dac8a613408faa3e66b47c105fdef0ed777b9dd4
Athiprat Kanha <103051972+AthipratKanha@users.noreply.github.com>
2022-04-05 23:16:57 +0700

    Add files via upload

---
+++ javasample1/java/code/service/Branch.java
@@ -0,0 +1,17 @@
+package service;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity @Table(name="branch")
+public class Branch {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	int number;
+	
+	String name;
+	double area;
+	
+	transient int staff;
+}
