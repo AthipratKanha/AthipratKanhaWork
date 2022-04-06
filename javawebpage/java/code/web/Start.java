10d1ad55f8cbe38e31e9e4c93d4e534d3737d5b8
Athiprat Kanha <103051972+AthipratKanha@users.noreply.github.com>
2022-04-06 20:49:56 +0700

    java

---
+++ javawebpage/java/code/web/Start.java
@@ -0,0 +1,22 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+
+class Start
+{
+    public static void main(String[] data)
+    {
+        ApplicationContext context;
+        context = SpringApplication.run(Initialize.class);
+    }
+}
+
+@SpringBootApplication
+class Initialize
+{
+    
+}
